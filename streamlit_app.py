29098eb87143ba050b0316c910517e02a4a11981
zyushin <59847009+zyushin@users.noreply.github.com>
2024-10-08 12:43:44 -0400

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import altair as alt
 import pandas as pd
 import streamlit as st
-
+#using template from steamline.io
 # Show the page title and description.
-st.set_page_config(page_title="Movies dataset", page_icon="🎬")
-st.title("🎬 Movies dataset")
+st.set_page_config(page_title="Movies dataset", page_icon="🌱")
+st.title("🌱 Carbon Offset Registries dataset")
 st.write(
     """
     This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
@@ -13,54 +13,53 @@ st.write(
     """
 )
 
-
 # Load the data from a CSV. We're caching this so it doesn't reload every time the app
 # reruns (e.g. if the user interacts with the widgets).
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/movies_genres_summary.csv")
+    df = pd.read_csv("data/ACR_CSV.csv")
     return df
 
 
 df = load_data()
 
 # Show a multiselect widget with the genres using `st.multiselect`.
-genres = st.multiselect(
-    "Genres",
-    df.genre.unique(),
-    ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"],
-)
+# genres = st.multiselect(
+#     "Genres",
+#     df.genre.unique(),
+#     ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"],
+# )
 
-# Show a slider widget with the years using `st.slider`.
-years = st.slider("Years", 1986, 2006, (2000, 2016))
-
-# Filter the dataframe based on the widget input and reshape it.
-df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
-df_reshaped = df_filtered.pivot_table(
-    index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
-)
-df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
-
-
-# Display the data as a table using `st.dataframe`.
-st.dataframe(
-    df_reshaped,
-    use_container_width=True,
-    column_config={"year": st.column_config.TextColumn("Year")},
-)
-
-# Display the data as an Altair chart using `st.altair_chart`.
-df_chart = pd.melt(
-    df_reshaped.reset_index(), id_vars="year", var_name="genre", value_name="gross"
-)
-chart = (
-    alt.Chart(df_chart)
-    .mark_line()
-    .encode(
-        x=alt.X("year:N", title="Year"),
-        y=alt.Y("gross:Q", title="Gross earnings ($)"),
-        color="genre:N",
-    )
-    .properties(height=320)
-)
-st.altair_chart(chart, use_container_width=True)
+# # Show a slider widget with the years using `st.slider`.
+# # years = st.slider("Years", 1986, 2006, (2000, 2016))
+#
+# # Filter the dataframe based on the widget input and reshape it.
+# df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
+# df_reshaped = df_filtered.pivot_table(
+#     index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
+# )
+# df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
+#
+#
+# # Display the data as a table using `st.dataframe`.
+# st.dataframe(
+#     df_reshaped,
+#     use_container_width=True,
+#     column_config={"year": st.column_config.TextColumn("Year")},
+# )
+#
+# # Display the data as an Altair chart using `st.altair_chart`.
+# df_chart = pd.melt(
+#     df_reshaped.reset_index(), id_vars="year", var_name="genre", value_name="gross"
+# )
+# chart = (
+#     alt.Chart(df_chart)
+#     .mark_line()
+#     .encode(
+#         x=alt.X("year:N", title="Year"),
+#         y=alt.Y("gross:Q", title="Gross earnings ($)"),
+#         color="genre:N",
+#     )
+#     .properties(height=320)
+# )
+# st.altair_chart(chart, use_container_width=True)
