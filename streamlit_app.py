29d3338b69943b2c91ac2521f3f97af9b092648c
zyushin <59847009+zyushin@users.noreply.github.com>
2024-10-09 02:46:02 -0400

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,49 +46,12 @@ df_selection = df.query(
     "Registry == @registry"
 )
 
-
-# # Show a slider widget with the years using `st.slider`.
-# # years = st.slider("Years", 1986, 2006, (2000, 2016))
-#
-# # Filter the dataframe based on the widget input and reshape it.
-# df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
-# df_reshaped = df_filtered.pivot_table(
-#     index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
-# )
-# df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
-#
-#
 # Display the data as a table using `st.dataframe`.
 st.dataframe(
     df_selection,
     use_container_width=True,
     # column_config={"year": st.column_config.TextColumn("Year")},
 )
-
-#sum sdg
-
-sdg_list = []
-sdg_dict = {}
-
-for v in range(df_selection['SDGs'].size):
-    text = df_selection['SDGs'].iloc[v]
-    pat = r'[1-9][1-9]*'
-    list = re.findall(pat,text)
-    list = [int(v) for v in list]
-    sdg_list.append(list)
-
-for v in range(len(sdg_list)):
-    for x in sdg_list[v]:
-        for key in range(len(sdg_dict)):
-            if x == key:
-                sdg_dict[key] += 1
-
-sdg_dict = {"Goal " + str(key): [value] for key, value in sdg_dict.items()}
-sdg_df = pd.DataFrame(sdg_dict)
-
-# stg_df = pd.read_csv("data/sdg.csv")
-#Bar Chart
-chart_data = sdg_df
 
 # Display the data as an Altair chart using `st.altair_chart`.
 # df_chart = pd.melt(
